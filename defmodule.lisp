7ad2106ac3113db17ff8373a87550c8383cce05f
Donl <sexdon@126.com>
2015-01-12 11:14:12 +0800

    source

---
+++ defmodule.lisp
@@ -0,0 +1,24 @@
+;;;; defmodule.lisp
+
+(restas:define-module #:authdemo
+  (:use #:cl #:restas)
+  (:export #:*authenticate-user-function*
+		   #:*register-user-function*
+		   #:*redirect-route*
+		   #:logged-on-p))
+
+(in-package #:authdemo)
+
+(defparameter *template-directory*
+  (merge-pathnames #P"templates/" authdemo-config:*base-directory*))
+
+(defparameter *static-directory*
+  (merge-pathnames #P"static/" authdemo-config:*base-directory*))
+
+(sexml:with-compiletime-active-layers
+  (sexml:standard-sexml sexml:xml-doctype)
+  (sexml:support-dtd
+	(merge-pathnames "html5.dtd" (asdf:system-source-directory "sexml"))
+	:<))
+
+
